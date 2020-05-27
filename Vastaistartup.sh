35b56949fb6651a73300f35afb8273944db1a548
sanchit2843 <sanchittanwar75@gmail.com>
2020-05-27 06:38:52 +0530

    Update Vastaistartup.sh

--- Vastaistartup.sh
+++ Vastaistartup.sh
@@ -2,3 +2,11 @@ apt-get update
 apt-get install libglib2.0-0
 apt-get install -y libsm6 libxext6 libxrender-dev
 pip install opencv-python
+
+#SSH config file to run vast ai with vscode
+Host vast_ai_instance
+    HostName ssh4.vast.ai
+    User root
+    PORT 17138
+    LOCALForward 8080 localhost:8080
+    IdentityFile C:\Users\sanchit\OneDrive\Documents\gen.pem
