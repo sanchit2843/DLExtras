652e18e69d7773ecaac91163b52b9cf485825ae4
sanchit2843 <sanchittanwar75@gmail.com>
2020-05-30 00:35:08 +0530

    Update Vastaistartup.sh

--- Vastaistartup.sh
+++ Vastaistartup.sh
@@ -3,10 +3,14 @@ apt-get install libglib2.0-0
 apt-get install -y libsm6 libxext6 libxrender-dev
 pip install opencv-python
 
+#oh my tmux installation
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
 #SSH config file to run vast ai with vscode
-Host vast_ai_instance
-    HostName ssh4.vast.ai
-    User root
-    PORT 17138
-    LOCALForward 8080 localhost:8080
-    IdentityFile C:\Users\sanchit\OneDrive\Documents\gen.pem
+# Host vast_ai_instance
+#     HostName ssh4.vast.ai
+#     User root
+#     PORT 17138
+#     LOCALForward 8080 localhost:8080
+#     IdentityFile C:\Users\sanchit\OneDrive\Documents\gen.pem
